63169579ce4065ca71c868d0f47837d7231aa8d1
Vannida Lim <vannida.lim@gmail.com>
2020-08-12 19:42:43 -0400

    Add boilerplate to Translate, Dropdown, and Convert

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef} from 'react';
 
-const Dropdown = ({ label, options, sselected, onSelectedChange}) => {
+const Dropdown = ({ label, options, selected, onSelectedChange}) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
 
+    
 
     return (
         <div>
-            from dd
+            <div className='ui form' ref={ref}>
+                <div className='field'>
+                    <label className='label'>{label}</label>
+                    <div
+                        onClick={() => setOpen(!open)}
+                        className={`ui seleciton dropdown ${open ? 'visible active' : ''}`}
+                    >
+                        <i className='dropdown icon'></i>
+                        <div className='text'>{selected.label}</div>
+                        <div className={`menu ${open ? 'visible transition' : ''}`}>{null}</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
--- src/Translate.js
+++ src/Translate.js
@@ -42,9 +42,11 @@ const Translate = () => {
     const [text, setText] = useState('');
     return (
         <div>
-            <div>
+            <div className='ui form'>
+                <div className='field'>
                 <label>Enter Text to Translate</label>
                 <input value={text} onChange={(e) => setText(e.target.value)}/>
+                </div>
             </div>
             <Dropdown 
                 label='Select a Language'
@@ -52,6 +54,7 @@ const Translate = () => {
                 onSelectedChange={setLanguage}
                 options={options}
             />
+            <h3 className='ui header'>Output</h3>
             <Convert text={text} language={language}/>
         </div>
     );
