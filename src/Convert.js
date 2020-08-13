3e50b5c4925318421e1e6c2de5589643212cf380
Vannida Lim <vannida.lim@gmail.com>
2020-08-13 09:34:22 -0400

    Fetch translation data from Google Translate API and config debouncedText

--- src/Convert.js
+++ src/Convert.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
 
-const Convert = () => {
+
+const Convert = ({ language, text }) => {
+    const [translated, setTranslated] = useState('');
+    const [debouncedText, setDebouncedText] = useState(text);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text);
+        }, 500);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
+
+    useEffect(() => {
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: debouncedText,
+                    target: language.value,
+                    key: GOOGLE_API_KEY
+                }
+            });
+            setTranslated(data.data.translations[0].translatedText);
+        };
+        doTranslation();
+    }, [language, debouncedText])
+
     return (
         <div>
-            form convert
+            <h1 className='ui header'>{translated}</h1>
         </div>
-    )
+    );
 };
 
 export default Convert; 
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -4,7 +4,32 @@ const Dropdown = ({ label, options, selected, onSelectedChange}) => {
     const [open, setOpen] = useState(false);
     const ref = useRef();
 
-    
+    useEffect(() => {
+        const onBodyClick = (e) => {
+            if (ref.current.contains(e.target)) {
+                return;
+            }
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    });
+
+    const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+        return (
+            <div key={option.value} className='item' onClick={() => onSelectedChange(option)}>
+                {option.label}
+            </div>
+        );
+    });
+
 
     return (
         <div>
@@ -17,12 +42,12 @@ const Dropdown = ({ label, options, selected, onSelectedChange}) => {
                     >
                         <i className='dropdown icon'></i>
                         <div className='text'>{selected.label}</div>
-                        <div className={`menu ${open ? 'visible transition' : ''}`}>{null}</div>
+                        <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 };
 
 export default Dropdown;
